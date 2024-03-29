62559dbc8d6cb709e9abefe9d7d550ba6f7493ba
Isaac Almanza <isaac.almanza19@gmail.com>
2024-03-29 18:04:35 -0500

    test: add additional test

--- test/Sender.t.sol
+++ test/Sender.t.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
 import { PRBTest } from "@prb/test/src/PRBTest.sol";
@@ -20,6 +20,7 @@ contract SenderTest is PRBTest, StdCheats {
     uint256 constant SENDER_PRIVATE_KEY = 2222;
     uint256 constant FEE = 10;
     uint constant initial_suppy = 1e12;
+    uint256 deadline = block.timestamp + 60;
 
     function setUp() public virtual {
         bob = vm.addr(SENDER_PRIVATE_KEY);
@@ -30,7 +31,7 @@ contract SenderTest is PRBTest, StdCheats {
 
     function testSent() public {
         uint amountLeft = initial_suppy - AMOUNT;
-        uint256 deadline = block.timestamp + 60;
+        
         //prepare typed message
         bytes32 permitHash = _getPermitHash(bob, address(gasless_sender), AMOUNT, usdc.nonces(bob), deadline);
 
@@ -45,12 +46,56 @@ contract SenderTest is PRBTest, StdCheats {
         assertEq(usdc.balanceOf(maria), AMOUNT, "receiver balance");
     }
 
+    function testRevert_OnlyGelatoRelayERC2771() public {
+         //prepare typed message
+        bytes32 permitHash = _getPermitHash(bob, address(gasless_sender), AMOUNT, usdc.nonces(bob), deadline);
+
+        //signed typed message
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(SENDER_PRIVATE_KEY, permitHash);
+
+        // maria trying to be the msg.sender
+        vm.prank(maria);
+        vm.expectRevert(Sender.OnlyGelatoRelayERC2771.selector);
+        gasless_sender.send(bob, maria, AMOUNT, deadline, v, r, s);
+    }
+
+    function testRevert_InvalidSenderAddress() public {
+         //prepare typed message
+        bytes32 permitHash = _getPermitHash(bob, address(gasless_sender), AMOUNT, usdc.nonces(bob), deadline);
+
+        //signed typed message
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(SENDER_PRIVATE_KEY, permitHash);
+
+        vm.expectRevert(Sender.InvalidSenderAddress.selector);
+        gasless_sender.send(address(0), maria, AMOUNT, deadline, v, r, s);
+    }
+
+    function testRevert_InvalidReceiverAddress() public {
+         //prepare typed message
+        bytes32 permitHash = _getPermitHash(bob, address(gasless_sender), AMOUNT, usdc.nonces(bob), deadline);
+
+        //signed typed message
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(SENDER_PRIVATE_KEY, permitHash);
+        vm.expectRevert(Sender.InvalidReceiverAddress.selector);
+        gasless_sender.send(bob, address(0), AMOUNT, deadline, v, r, s);
+    }
+
+    function testRevert_TransferAmount() public {
+         //prepare typed message
+        bytes32 permitHash = _getPermitHash(bob, address(gasless_sender), AMOUNT, usdc.nonces(bob), deadline);
+
+        //signed typed message
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(SENDER_PRIVATE_KEY, permitHash);
+        vm.expectRevert(Sender.TransferAmountMustBeGreaterThanZero.selector);
+        gasless_sender.send(bob, maria, 0, deadline, v, r, s);
+    }
+
     function _getPermitHash(
-        address owner,
-        address spender,
-        uint256 value,
-        uint256 nonce,
-        uint256 deadline
+        address _owner,
+        address _spender,
+        uint256 _value,
+        uint256 _nonce,
+        uint256 _deadline
     )
         private
         view
@@ -63,11 +108,11 @@ contract SenderTest is PRBTest, StdCheats {
                 keccak256(
                     abi.encode(
                         keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"),
-                        owner,
-                        spender,
-                        value,
-                        nonce,
-                        deadline
+                        _owner,
+                        _spender,
+                        _value,
+                        _nonce,
+                        _deadline
                     )
                 )
             )
